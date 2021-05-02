41c34eecec3f7d19028fbbfa4cf818b03048a4e6
jsjackson <jsjackson@example.com>
2021-05-02 21:42:48 +1000

    remove sample services and add APIGateway service & Lambda service

--- lib/cdk-typescript-stack.ts
+++ lib/cdk-typescript-stack.ts
@@ -1,18 +1,22 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
 
 export class CdkTypescriptStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkTypescriptQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    //define an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHander', {
+      runtime: lambda.Runtime.NODEJS_10_X,  //execution environment
+      code: lambda.Code.fromAsset('lambda'), //code loaded from "lambda" directory
+      handler: 'hello.handler'  //file name is "hello", function name is "handler"
+    });
+ 
+    //define an API Gateway REST API resource backed by our "hello" function
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
     });
 
-    const topic = new sns.Topic(this, 'CdkTypescriptTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }

3f1fbd418f31f693214fc5aa1c9dfee732138e8c
jsjackson <jsjackson@example.com>
2021-05-02 22:51:17 +1000

    Add HitCounter API & HitCounter handler

--- lib/cdk-typescript-stack.ts
+++ lib/cdk-typescript-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigw from '@aws-cdk/aws-apigateway';
+import { HitCounter } from './hitcounter';
 
 export class CdkTypescriptStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -13,9 +14,14 @@ export class CdkTypescriptStack extends cdk.Stack {
       handler: 'hello.handler'  //file name is "hello", function name is "handler"
     });
  
+    // the hitcounter
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
+
     //define an API Gateway REST API resource backed by our "hello" function
     new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello
+      handler: helloWithCounter.handler
     });
 
   }
